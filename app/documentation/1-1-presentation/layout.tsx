861ee979326a7c4405b31e7cc9922524b8ecb690
margoul1Malin <github.pydoctor@gmail.com>
2025-09-26 21:46:42 +0200

    new commit qsdqsdsdaaaaasdsdsdsdsdsdsdsdsdsdssdsdsdsdsdsqdqssdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdzze1111sd891235678910111234esdsdsddsdsdzedsddssddsdssdsddsdsdsdssd

---
+++ app/documentation/1-1-presentation/layout.tsx
@@ -0,0 +1,58 @@
+import type { Metadata, Viewport } from "next";
+
+export const metadata: Metadata = {
+  title: "Présentation -Chantier Direct",
+  description: "Présentation de Chantier Direct",
+  keywords: "Chantier Direct, présentation, documentation",
+  authors: [{ name: "Chantier Direct" }],
+  publisher: "Chantier Direct",
+  icons: {
+    icon: "/ChantierDirectIcon.png",
+  },
+  openGraph: {
+    images: [
+        {
+            url : "/ChantierDirectIcon.png",
+            width: 1200,
+            height: 630,
+            alt: "Présentation - Chantier Direct",
+        },
+    ],
+    title: "Présentation - Chantier Direct",
+    description: "Présentation de Chantier Direct",
+    url: "https://www.chantier-direct.fr",
+    siteName: "Chantier Direct",
+    locale: "fr_FR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Présentation - Chantier Direct",
+    description: "Présentation de Chantier Direct",
+    images: "/ChantierDirectIcon.png",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: "https://www.chantier-direct.fr/documentation/1-1-presentation",
+  },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
+export default function Documentation11PresentationLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="">
+        {children}
+    </main>
+  );
+}
